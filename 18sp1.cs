2438a0de209200eb458de430190df763338b10a9
togherret-coder <togherret@gmail.com>
2025-12-03 22:19:36 +0300

    Add files via upload

---
+++ 18sp1.cs
@@ -0,0 +1,66 @@
+﻿//1(1)
+Console.WriteLine("1 задание");
+string a;
+a = (Console.ReadLine());
+int b=int.Parse(a)+100;
+Console.WriteLine(b);
+//1(2)
+Console.WriteLine("2 задание");
+double c = (double)(7 / 2);
+Console.WriteLine(c);
+int d = (int)(7.0 / 2);
+Console.WriteLine(d);
+//1(3)
+Console.WriteLine("3 задание");
+string e= (Console.ReadLine());
+if (int.TryParse(e, out int newNumber)) {Console.WriteLine(newNumber*newNumber);}
+else { Console.WriteLine("Ошибка ввода"); }
+//1(4)
+Console.WriteLine("4 задание");
+double f = double.Parse(Console.ReadLine());
+int g = (int)(f);
+Console.WriteLine(f);
+Console.WriteLine(g);
+//5 
+Console.WriteLine("5 задание");
+int[] h = new int[5] { 1, 414, 88, 78, 70 };
+string i="";
+for (int j = 0; j < 5; j++)
+{ i = i + h[j]+","; }
+Console.WriteLine(i);
+//6
+Console.WriteLine("6 задание");
+int l = 0;
+for  (int j = 0; j < 5; j++)
+{ if (h[j] % 2 == 0) { l++; } }
+Console.WriteLine(l);
+//7
+Console.WriteLine("7 задание");
+int k = 0;
+for (int j = 0;j < 5; j++) { k=k+h[j]; }
+Console.WriteLine(k);
+Console.WriteLine((float)k/h.Length);
+//8
+Console.WriteLine("8 задание");
+int m = h[0];
+h[0] = h[h.Length-1];
+h[h.Length-1] = m;
+string n = "";
+for (int j = 0; j < 5; j++)
+{ n = n + h[j]+","; }
+Console.WriteLine(n);
+//9
+Console.WriteLine("9 задание");
+int min = h[0];
+int minind = 0;
+int max = h[0];
+int maxind = 0;
+for (int j = 1; j < 5; j++)
+{
+    if (min >= h[j]) { min = h[j];minind = j; }
+    if (max <= h[j]) { max = h[j]; maxind = j; }
+}
+Console.WriteLine(min);
+Console.WriteLine(minind);
+Console.WriteLine(max);
+Console.WriteLine(maxind);
---
+++ 18sp2.cs
@@ -0,0 +1,47 @@
+﻿
+//11
+Console.WriteLine("11 задание");
+int r = int.Parse(Console.ReadLine());
+while (r > jor.Length - 1) { r = int.Parse(Console.ReadLine()); }
+jor[r] = 0;
+for (int j = r; j < 20; j++) { jor[j] = jor[j + 1]; }
+string n2 = "";
+for (int j = 0; j < 20; j++)
+{ n2 = n2 + jor[j] + ","; }
+Console.WriteLine(n2);
+//12
+Console.WriteLine("12 задание");
+int kl = int.Parse(Console.ReadLine());
+while (kl > jor.Length - 1) { kl = int.Parse(Console.ReadLine()); }
+jor[kl] = jor[jor.Length - 1];
+int[] jk = new int[jor.Length - 1];
+for (int j = r; j < 19; j++) { jk[j] = jor[j]; }
+string n3 = "";
+for (int j = 0; j < 20; j++)
+{ n3 = n3 + jk[j] + ","; }
+Console.WriteLine(n3);
+//13
+Console.WriteLine("13 задание");
+int x = int.Parse(Console.ReadLine());
+int op = 0;
+for (int j = 0; j < o.Length; j++) { if (o[j] == x) { op++; o[j] = 0; } }
+int[] yuh = new int[o.Length - op];
+for (int j = 0; j < o.Length; j++) { if (o[j] != 0) { yuh[j] = o[j]; } }
+string n5 = "";
+for (int j = 0; j < yuh.Length; j++)
+{ n = n5 + yuh[j] + ","; }
+Console.WriteLine(n5);
+//14
+Console.WriteLine("14 задание");
+int p = 0;
+for (int j = 0; j < h.Length / 2; j++)
+{ p = h[j]; h[j] = h[h.Length - 1 - j]; h[h.Length - 1 - j] = p; }
+string q = "";
+for (int j = 0; j < 5; j++)
+{ q = q + h[j] + ","; }
+Console.WriteLine(q);
+//15
+Console.WriteLine("15 задание");
+int[] jkl = new int[5] { 1, 3, 5, 2, 1 };
+for (int j = 0; j < 5; j++) { if (jkl[j] == jkl[jkl.Length - j - 1]) { }; }
+
